60fbaeb67f5a2860ac3716a8c514df44427bed41
selim görmüş <info@siberfx.com>
2025-10-04 16:43:14 +0300

    feat: add Dutch organization type variants and WOO task legal entity type

--- app/Enums/OrganizationType.php
+++ app/Enums/OrganizationType.php
@@ -4,18 +4,24 @@ namespace App\Enums;
 
 enum OrganizationType: string
 {
-    case MUNICIPALITY = 'gemeenten';
-    case PROVINCE = 'provincies';
-    case WATER_BOARD = 'waterschappen';
+    case MUNICIPALITY = 'municipality';
+    case GEMEENTE = 'gemeente';
+    case PROVINCE = 'province';
+    case PROVINCIE = 'provincie';
+    case WATER_BOARD = 'water_board';
+    case WATERSCHAP = 'waterschap';
     case REGIONAL_ASSOCIATION = 'regionale_samenwerkingsorganen';
     case CROSS_BORDER_REGIONAL_ASSOCIATION = 'grensoverschrijdende_regionale_samenwerkingsorganen';
     case PUBLIC_BODY = 'openbare_lichamen_voor_beroep_en_bedrijf';
     case HIGH_COUNCIL_OF_STATE = 'hoge_colleges_van_stat';
-    case MINISTRY = 'ministeries';
+    case MINISTRY = 'ministry';
+    case MINISTERIE = 'ministerie';
     case AGENCY = 'agentschappen';
     case INSPECTION = 'inspecties';
     case ADVISORY_BOARD = 'adviescolleges';
-    case INDEPENDENT_ADMINISTRATIVE_BODY = 'zelfstandige_bestuursorganen';
+    case INDEPENDENT_ADMINISTRATIVE_BODY = 'independent_administrative_body';
+    case ZELFSTANDIG_BESTUURSORGAAN = 'zelfstandig_bestuursorgaan';
+    case RECHTSPERSOON_MET_WOO_TAAK = 'rechtspersoon_met_woo_taak';
     case POLICE = 'politie';
     case JUDICIAL = 'rechtspraak';
 
@@ -26,18 +32,19 @@ enum OrganizationType: string
     {
 
         return match ($this) {
-            self::MUNICIPALITY => 'Gemeenten',
-            self::PROVINCE => 'Provincies',
-            self::WATER_BOARD => 'Waterschappen',
+            self::MUNICIPALITY, self::GEMEENTE => 'Gemeenten',
+            self::PROVINCE, self::PROVINCIE => 'Provincies',
+            self::WATER_BOARD, self::WATERSCHAP => 'Waterschappen',
             self::REGIONAL_ASSOCIATION => 'Regionale samenwerkingsorganen',
             self::CROSS_BORDER_REGIONAL_ASSOCIATION => 'Grensoverschrijdende regionale samenwerkingsorganen',
             self::PUBLIC_BODY => 'Openbare lichamen voor beroep en bedrijf',
             self::HIGH_COUNCIL_OF_STATE => 'Hoge Colleges van Staat',
-            self::MINISTRY => 'Ministeries',
+            self::MINISTRY, self::MINISTERIE => 'Ministeries',
             self::AGENCY => 'Agentschappen',
             self::INSPECTION => 'Inspecties',
             self::ADVISORY_BOARD => 'Adviescolleges',
-            self::INDEPENDENT_ADMINISTRATIVE_BODY => 'Zelfstandige bestuursorganen',
+            self::INDEPENDENT_ADMINISTRATIVE_BODY, self::ZELFSTANDIG_BESTUURSORGAAN => 'Zelfstandige bestuursorganen',
+            self::RECHTSPERSOON_MET_WOO_TAAK => 'Rechtspersoon met WOO-taak',
             self::POLICE => 'Politie',
             self::JUDICIAL => 'Rechtspraak',
         };
@@ -60,18 +67,19 @@ enum OrganizationType: string
     public function urlSegment(): string
     {
         return match ($this) {
-            self::MUNICIPALITY => 'Gemeenten',
-            self::PROVINCE => 'Provincies',
-            self::WATER_BOARD => 'Waterschappen',
+            self::MUNICIPALITY, self::GEMEENTE => 'Gemeenten',
+            self::PROVINCE, self::PROVINCIE => 'Provincies',
+            self::WATER_BOARD, self::WATERSCHAP => 'Waterschappen',
             self::REGIONAL_ASSOCIATION => 'Regionale_samenwerkingsorganen',
             self::CROSS_BORDER_REGIONAL_ASSOCIATION => 'Grensoverschrijdende_regionale_samenwerkingsorganen',
             self::PUBLIC_BODY => 'Openbare_lichamen_voor_beroep_en_bedrijf',
             self::HIGH_COUNCIL_OF_STATE => 'Hoge_Colleges_van_Staat',
-            self::MINISTRY => 'Ministeries',
+            self::MINISTRY, self::MINISTERIE => 'Ministeries',
             self::AGENCY => 'Agentschappen',
             self::INSPECTION => 'Inspecties',
             self::ADVISORY_BOARD => 'Adviescolleges',
-            self::INDEPENDENT_ADMINISTRATIVE_BODY => 'Zelfstandige_bestuursorganen',
+            self::INDEPENDENT_ADMINISTRATIVE_BODY, self::ZELFSTANDIG_BESTUURSORGAAN => 'Zelfstandige_bestuursorganen',
+            self::RECHTSPERSOON_MET_WOO_TAAK => 'Rechtspersoon_met_WOO_taak',
             self::POLICE => 'Politie',
             self::JUDICIAL => 'Rechtspraak',
         };
